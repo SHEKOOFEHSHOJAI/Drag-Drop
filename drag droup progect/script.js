fbc139e5d8b375ba33d302cd9d9b05211c803576
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-04-06 07:20:31 -0700

    shekoofeh-shojai-classA

--- drag droup progect/script.js
+++ drag droup progect/script.js
@@ -30,6 +30,7 @@ function createList() {
       .forEach((person, index) => {
         console.log(person);
         const listItem = document.createElement("li");
+      
 
         listItem.setAttribute("data-index", index);
 
