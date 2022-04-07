1399c1f62308480b5068a9108a14e9acf5d46f5d
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-04-07 00:32:25 -0700

    shekoofeh-shojai-classA

--- drag droup progect/script.js
+++ drag droup progect/script.js
@@ -28,6 +28,7 @@ function createList() {
       .sort((a, b) => a.sort - b.sort)
       .map((a) => a.value)
       .forEach((person, index) => {
+
         console.log(person);
         const listItem = document.createElement("li");
       
@@ -50,20 +51,101 @@ function createList() {
       addEventListener()
 }
 
-// drags etc
+//////////////////////
+// DRAGS etc
+/////////////////////
+
+//FUNCTION dragStart
+
+function dragStart() {
+
+// console.log('Event','dragstart');
+dragStartIndex = +this.closest("li").getAttribute("data-index");
+console.log(this.closest("li"));
+console.log(dragStartIndex);
+}
+
+//FUNCTION dragEnter
+function dragEnter() {
+  console.log("Event", "dragEnter");
+  this.classList.add("over");
+}
+
+//FUNCTION dragOver
+
+function dragOver(e) {
+  console.log("Event", "dragOver");
+   this.classList.remove("over");
+   e.preventDefault()
+}
+
+//FUNCTION dragLeave
+function dragLeave() {
+  console.log("Event", "dragLeave");
+}
+
+//FUNCTION DROP
+
+function dragDrop() {
+  console.log("Event", "dragDrop");
+  const dragEndIndex = +this.getAttribute("data-index");
+  swapItems(dragStartIndex,dragEndIndex)
+  this.classList.remove('over')
+}
+
+// Swap list items that are drag and drop
+
+function swapItems(fromIndex,toIndex) {
+
+const itemOne = listItems[fromIndex].querySelector(".draggable");
+const itemTwo = listItems[toIndex].querySelector(".draggable");
+
+// move index and 
+listItems[toIndex].appendChild(itemOne);
+listItems[fromIndex].appendChild(itemTwo);
+
+}
+
+// Check the order of list item
+
+check.addEventListener("click", checkOrder);
+
+//FUNCTION checkOrder
+function checkOrder() {
+
+listItems.forEach((listitem,index)=>{
+  const personeName = listitem.querySelector(".draggable")
+  .innerText.trim();
+
+  if (personeName !== richestPeple[index]) {
+
+    listitem.classList.add("wrong");
+  }
+   else {
+    listitem.classList.remove("wrong");
+    listitem.classList.add("right");
+  }
+
+})
+
+}
+
 
 function addEventListener(){
     const dragables = document.querySelectorAll(".draggable");
-      const dragListItems = document.querySelectorAll(".draggable li"); 
+      const dragListItems = document.querySelectorAll(".drggable-list  li"); 
 
        dragables.forEach(draggable=>{
+
            draggable.addEventListener("dragstart", dragStart);
        })
 
         dragListItems.forEach((item) => {
-          item.addEventListener("dragstart",dragStart);
-          item.addEventListener('drop',dragDrop)
-          item.addEventListener("dragenter", dragEnter);
-         item.addEventListener("dragleave", dragLeave);
+
+           item.addEventListener("dragover", dragOver);
+           item.addEventListener("dragenter", dragEnter);
+           item.addEventListener("dragleave", dragLeave);
+           item.addEventListener("drop", dragDrop);
+
         });
 }
